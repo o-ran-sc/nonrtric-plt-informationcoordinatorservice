f48a91efc2483ca72bb3f48eef0baa59ecd0a5a6
PatrikBuhr <patrik.buhr@est.tech>
2022-10-07 14:22:31 +0200

    NONRTRIC - added NBI feature
    Remove all jobs for one producer.
    
    Signed-off-by: PatrikBuhr <patrik.buhr@est.tech>
    Issue-ID: NONRTRIC-810
    Change-Id: I02860756c9703d36fbf8b31ab262c1994988b3a5

--- src/test/java/org/oransc/ics/ApplicationTest.java
+++ src/test/java/org/oransc/ics/ApplicationTest.java
@@ -221,6 +221,23 @@ class ApplicationTest {
     }
 
     @Test
+    void consumerDeleteJobsForOneOwner() throws Exception {
+        putInfoProducerWithOneType("producer1", TYPE_ID);
+        putInfoJob(TYPE_ID, "jobId1");
+        putInfoJob(TYPE_ID, "jobId2");
+        assertThat(this.infoJobs.size()).isEqualTo(2);
+        String url = ConsumerConsts.API_ROOT + "/info-jobs?owner=owner";
+        restClient().delete(url).block();
+        assertThat(this.infoJobs.size()).isZero();
+
+        assertThat(this.producerSimulator.getTestResults().jobsStarted.size()).isEqualTo(2);
+
+        await()
+            .untilAsserted(() -> assertThat(this.producerSimulator.getTestResults().jobsStopped.size()).isEqualTo(2));
+
+    }
+
+    @Test
     void a1eGetEiTypesEmpty() throws Exception {
         String url = A1eConsts.API_ROOT + "/eitypes";
         String rsp = restClient().get(url).block();
